351b623a510ee85549ac2bb52b7b76d819fb077b
RizkiputraRamadhan <poetraarromadhon56@gmail.com>
2023-11-20 23:21:16 +0700

    ujian WP2 Rizki PR

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.4.2/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <div class="w-full shadow-inner">
+        @include('layouts.navbar')
+        @yield('content')
+    </div>
+    <footer class="footer footer-center mt-5 p-4 bg-base-200 text-base-content">
+        <aside>
+          <p>Copyright © 2023 - Created by Rizki Putra Ramadhan 12221436</p>
+        </aside>
+      </footer>
+</body>
+<script>
+    function refreshPage() {
+        location.reload(true);
+    }
+</script>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\employeeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/employee', [employeeController::class, 'index']);
+Route::get('/employee/create', [employeeController::class, 'create']);
+Route::post('/employee/store', [employeeController::class, 'store']);
+Route::get('/employee/{id}/{nama}', [employeeController::class, 'show']);
+Route::delete('/employee/{id}/delete', [employeeController::class, 'destroy']);
